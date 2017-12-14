ac488da3a78cf940f670755628ef0780e98a6795
Bart Massey <bart@cs.pdx.edu>
2017-12-14 02:08:55 -0800

    added cylon example

---
+++ examples/cylon.rs
@@ -0,0 +1,36 @@
+// Copyright © 2017 Bart Massey
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
+//! ASCII-art "Cylon" display.
+
+//! This is something we used to write in the 1980s to run
+//! on our glass TTYs. It is a reasonable demo of some of
+//! the package features.
+
+#[macro_use]
+extern crate prompted;
+
+use std::str;
+use std::thread::sleep;
+use std::time::Duration;
+
+const WIDTH: usize = 7;
+
+pub fn main() {
+    let mut chars = [b' ';WIDTH];
+    chars[0] = b'*';
+    let mut posn = 0;
+    let mut dirn = -1isize;
+    loop {
+        chars[posn] = b' ';
+        if posn == 0 || posn == WIDTH - 1 {
+            dirn = -dirn
+        };
+        posn = (posn as isize + dirn) as usize;
+        chars[posn] = b'*';
+        print_flush!("{}\r", str::from_utf8(&chars).unwrap());
+        sleep(Duration::from_millis(100));
+    }
+}
