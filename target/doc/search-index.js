ce10229692a8149bd62bff858f2dc4583d3db104
Bart Massey <bart.massey@gmail.com>
2023-02-15 16:35:46 -0800

    deleted target directory containing docs

--- target/doc/search-index.js
+++
@@ -1,4 +0,0 @@
-var searchIndex = JSON.parse('{\
-"prompted":{"doc":"Prompting macros.","t":[5,14,5,14,14,5],"n":["eflush","eprompt","flush","input","prompt","read_line"],"q":["prompted","","","","",""],"d":["Flush standard error. Intended primarily for use by macros.","Same functionality as <code>prompt!()</code> except using <code>stderr()</code> …","Flush standard output. Intended primarily for use by …","If a <code>[format!]</code> describing a prompt is present, print it on …","Same functionality as <code>print!()</code> except that <code>stdout()</code> is …","Read a line from standard input. Removes a trailing …"],"i":[0,0,0,0,0,0],"f":[[[]],null,[[]],null,null,[[],["string",3]]],"p":[]}\
-}');
-if (window.initSearch) {window.initSearch(searchIndex)};
